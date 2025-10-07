b1ae00d0d015f74f1729c50faae5522168a7669b
APrescott24 <aaronprescott2012@hotmail.co.uk>
2025-10-07 15:36:49 +0100

    chore: update week 1 lab

--- OneDrive - Edge Hill University/week1_lab.py
+++ OneDrive - Edge Hill University/week1_lab.py
@@ -1,9 +1,8 @@
 from ast import Add
 
+student_name = "Aaron"
 
-student_name = "Alex"
-
-student_age = 20
+student_age = 26
 
 current_year = 2025
 
@@ -13,7 +12,3 @@ print("Student Age:", student_age)
 YOB = current_year - student_age
 
 print("Year of Birth:", YOB)
-
-git add week1_lab.py
-git commit -m "Feat: Completed week 1 lab exercises"
-git push
