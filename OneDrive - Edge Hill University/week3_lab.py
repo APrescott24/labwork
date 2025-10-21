eb7dbd49fb17dda6ec9f5f238a717692c0ffe53a
APrescott24 <aaronprescott2012@hotmail.co.uk>
2025-10-21 14:25:40 +0100

    print only even numbers from 2 to 20
    modified to only print the even numbers between 2 and 20

--- OneDrive - Edge Hill University/week3_lab.py
+++ OneDrive - Edge Hill University/week3_lab.py
@@ -1,5 +1,3 @@
 #Program that prints the numbers from 1 to 10
 for i in range(10):
-    print(f"Number {i+1}") 
-
-
+    print(f"Number {i+i+2}")  #modified to only print the even numbers between 2 and 20
